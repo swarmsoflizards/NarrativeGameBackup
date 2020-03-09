ed4694eac7a8c71fc0825308c985e1bfb8372f2d
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-03-08 22:17:04 -0500

    Created DetectLookAtInteractive.cs

---
+++ Assets/Scripts/DetectLookAtInteractive.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectLookAtInteractive : MonoBehaviour
+{
+    [Tooltip("Starting point of raycast used to detect interactives.")]
+    [SerializeField] private Transform raycastOrigin;
+    [Tooltip("How far from the raycastOrigin to search for interactive elements.")]
+    [SerializeField]float maxRange = 5.0f;
+
+    private Vector3 raycastDirection;
+
+    private void FixedUpdate()
+    {
+        Physics.Raycast(raycastOrigin.position, raycastDirection, maxRange); 
+    }
+
+}
