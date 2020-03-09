469b55a90c8c1e3633fb32b888d403c7cb5a5db1
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-03-09 13:14:53 -0500

    Wrote InteractiveObject.cs

---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour, IInteractive
+{
+    public void InteractWith()
+    {
+        Debug.Log($"Player has interacted with (gameObject.name).");
+    }
+}
