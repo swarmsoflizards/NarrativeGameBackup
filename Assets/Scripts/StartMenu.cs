ab5d50843955d7fc8d480c25cb1fb5d0fcb3f794
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-04-06 15:00:57 -0500

    Added StartMenu and GameScene to build settings

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// This script contains the methods used by the buttons in the start menu
+    /// </summary>
+
+    [SerializeField] private string gameSceneName;
+
+    public void LoadGameScene()
     {
-        
+        SceneManager.LoadScene(gameSceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Debug.Log("Quit game");
+        Application.Quit();
     }
 }
