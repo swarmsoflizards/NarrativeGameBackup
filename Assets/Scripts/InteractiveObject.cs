8e1ddfd0f1c655dedb14a9a6af398bf7ea913291
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-04-14 13:54:29 -0500

    Configuring object text in InteractiveObject.cs

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
+    [Tooltip("Where the interactive object text should display")]
+    [SerializeField] private TextMeshProUGUI DisplayText;
+
+    [Tooltip("What the interactive object's text should display")]
+    [SerializeField] private string InteractiveObjectText;
+
     public void InteractWith()
     {
+        DisplayText.text = InteractiveObjectText;
         Debug.Log($"Player has interacted with (gameObject.name).");
     }
 }
