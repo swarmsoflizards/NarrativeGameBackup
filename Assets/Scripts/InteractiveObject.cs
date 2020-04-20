c21cf389fa8216f6319dbf6f0f2728b2c797175f
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-04-20 14:39:21 -0500

    Configured sfx for interactive object

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -8,9 +8,16 @@ public class InteractiveObject : MonoBehaviour, IInteractive
     [SerializeField] private string displayText = nameof(InteractiveObject); //Set display text in editor
 
     public string DisplayText => displayText; //Take DisplayText from IInteractive and set it to displayText 
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void InteractWith()
     {
+        audioSource.Play();
         Debug.Log($"Player has interacted with (gameObject.name).");
     }
 }
