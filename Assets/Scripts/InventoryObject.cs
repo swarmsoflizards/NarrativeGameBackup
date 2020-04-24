37ccde7123b7eca5b6430ddf9ff32d85974226f5
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-04-24 16:28:16 -0500

    Disbales mesh renderer and collider of inventory object on interact

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -7,6 +7,15 @@ public class InventoryObject : InteractiveObject
     //TODO: Add long description field
     //TODO: Add icon field
 
+    private new Renderer renderer;
+    private new Collider collider;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+    }
+
     public InventoryObject()
     {
         displayText = nameof(InventoryObject);
@@ -16,11 +25,13 @@ public class InventoryObject : InteractiveObject
     /// When the player interactis with an inventory object, we need to:
     /// 1. Add the inventory object to the PlayerInventory list
     /// 2. Remove the object from the game world / scene
+    ///     Disable collider and renderer
      /// </summary>
     public override void InteractWith()
     {
         base.InteractWith();
         PlayerInventory.InventoryObjects.Add(this);
-
+        renderer.enabled = false;
+        collider.enabled = false;
     }
 }
