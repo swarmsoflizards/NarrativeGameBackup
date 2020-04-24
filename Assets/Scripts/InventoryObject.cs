b4781947c610c78b5a18663273e7b781fea47ab8
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-04-24 15:35:08 -0500

    Created inventory scripts

---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : InteractiveObject
+{
+    //TODO: Add long description field
+    //TODO: Add icon field
+}
