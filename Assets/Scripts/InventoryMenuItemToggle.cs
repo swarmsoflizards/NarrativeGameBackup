e501e4a0ee7f41b3975b2261ef8c105f081cc223
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-04-26 15:51:25 -0500

    Configured icon image, applied to inventory item prefab

--- Assets/Scripts/InventoryMenuItemToggle.cs
+++ Assets/Scripts/InventoryMenuItemToggle.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryMenuItemToggle : MonoBehaviour
 {
-    
+    [Tooltip("The image component used to show the associated object's icon")]
+    [SerializeField] private Image iconImage;
+    private InventoryObject associatedInventoryObject;
 }
