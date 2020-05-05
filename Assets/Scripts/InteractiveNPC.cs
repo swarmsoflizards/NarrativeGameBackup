fde5c7aecfed52ef34ebc6015de792268e475f9b
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-05-05 16:17:57 -0500

    Began writing InteractiveNPC.cs

--- Assets/Scripts/InteractiveNPC.cs
+++ Assets/Scripts/InteractiveNPC.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class InteractiveNPC : MonoBehaviour
+/// <summary>
+/// This script controlls the behavior of NPCs, which are just different Interactive Objects.
+/// They need to:
+/// 1. Display dialogue text when interacted with
+/// 2. Cycle through that dialogue text when interacted with again
+/// 3. Be able to be instantiated in another location
+/// 4. Track the number of interactions the player has had with it
+/// </summary>
+public class InteractiveNPC : InteractiveObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("The character's name, to display on raycast")]
+    [SerializeField] private string NPCName;
+
+    [Tooltip("AudioClip that plays when the player interacts with NPC")]
+    [SerializeField] private AudioClip audioClip;
+
+    private int numberOfInteractions = 0;
+
+    public InteractiveNPC()
     {
-        
+        displayText = NPCName;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void InteractWith()
     {
-        
+        numberOfInteractions++;
+        Debug.Log("Number of interactions with " + NPCName + ": " + numberOfInteractions);
+        SpeakDialogue();
+        TakeResponse();
+        base.InteractWith();
+    }
+
+    private void SpeakDialogue()
+    {
+
+    }
+
+    private void TakeResponse()
+    {
+
     }
 }
