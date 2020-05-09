ce1d61a8de282b456b24a590a682d98ce7e674a7
Dan Steinmetz <emma.steinmetz@gmail.com>
2020-05-09 17:18:01 -0500

    Configured ambient sound and end menu

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -7,12 +7,15 @@ public class EndMenu : MonoBehaviour
 {
     [Tooltip("Start menu scene")]
     [SerializeField] private string menuSceneName;
+    [Tooltip("End menu panel")]
+    [SerializeField] private GameObject endMenuCanvas;
 
     private AudioSource audioSource;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        endMenuCanvas.SetActive(false);
     }
 
     public void PlaySoundEffect()
